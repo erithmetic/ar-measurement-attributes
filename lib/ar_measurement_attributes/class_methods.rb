ce9330dca9e910dcc12a8d58b438bda16c848987
Derek Kastner <dkastner@gmail.com>
2009-12-26 14:27:35 -0500

    Added ClassMethods

---
+++ lib/ar_measurement_attributes/class_methods.rb
@@ -0,0 +1,26 @@
+module ARMeasurementAttributes
+  # Class methods that are inserted into ActiveRecord::Base.  
+  # DSL Methods are automagically created for each measurement 
+  # type defined in ARMeasurementAttributes::DEFAULT_MEASUREMENTS.
+  # The methods defined here are simply placeholders for 
+  # documentation.
+  # To customize the types of measurements that can be defined,
+  # see ARMeasurementAttributes::DEFAULT_MEASUREMENTS
+  module ClassMethods
+    def area(name, custom_options = {}); end
+    def biomass(name, custom_options = {}); end
+    def cost(name, custom_options = {}); end
+    def electrical_energy(name, custom_options = {}); end
+    def length(name, custom_options = {}); end
+    def length_per_volume(name, custom_options = {}); end
+    def mass(name, custom_options = {}); end
+    def percentage(name, custom_options = {}); end
+    def speed(name, custom_options = {}); end
+    def time(name, custom_options = {}); end
+    def volume(name, custom_options = {}); end
+  end
+end
+
+ARMeasurementAttributes::DEFAULT_MEASUREMENTS.keys.each do |measurement|
+  ARMeasurementAttributes::DSL.add_class_method(measurement)
+end

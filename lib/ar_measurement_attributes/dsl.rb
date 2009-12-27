6317fc7115077be135e4e1c7531e99330fd6768b
Derek Kastner <dkastner@gmail.com>
2009-12-27 15:42:30 -0500

    Added documentation for DSL

--- lib/ar_measurement_attributes/dsl.rb
+++ lib/ar_measurement_attributes/dsl.rb
@@ -1,6 +1,7 @@
 module ARMeasurementAttributes
   class DSL
     class << self
+      # Adds a new type of measurement to the DSL
       def add_class_method(measurement)
         ARMeasurementAttributes::ClassMethods.send(:define_method, measurement) do |name, *args|
           custom_options = args.first || {}
@@ -8,11 +9,20 @@ module ARMeasurementAttributes
         end 
       end
 
+      # Dynamically creates getters and setters for a named measurement
+      # defined by the user's ActiveRecord definition
+      #
+      # Example:
+      #  class Sauce < ActiveRecord::Base
+      #    mass :meat_mass
+      #  end
+      # Will create a getter and setter for a new #meat_mass attribute
       def create_attribute(target_klass, measurement, name, options = {})
         define_getter(target_klass, measurement, name, options)
         define_setter(target_klass, measurement, name, options)
       end
 
+      # Dynamically creates a getter for a named measurement (see #create_attribute)
       def define_getter(target_klass, measurement, name, defined_options = {})
         target_klass.send(:define_method, name) do |*args|
           runtime_options = args.first || {}
@@ -21,6 +31,7 @@ module ARMeasurementAttributes
         end
       end
 
+      # Dynamically creates a setter for a named measurement (see #create_attribute)
       def define_setter(target_klass, measurement, base_name, default_options = {})
         name = "#{base_name}=".to_sym
         target_klass.send(:define_method, name) do |value|

2c493ff5a8e7aa95eed63129b4dead8fcab1c2d9
Derek Kastner <dkastner@gmail.com>
2009-12-27 17:57:35 -0500

    Changed my mind. Using ar_object.pretty_attribute rather than ar_object.attribute(:pretty).  It's clearer and more API-user friendly

--- integration/spec/integration_spec.rb
+++ integration/spec/integration_spec.rb
@@ -5,10 +5,10 @@ describe 'initialization' do
     ActiveRecord::Base.should be_a_kind_of(ARMeasurementAttributes::ClassMethods)
   end
 end
-__END__
+
 describe ARMeasurementAttributes do
   class Automobile < ActiveRecord::Base
-    percentage :urbanity
+    percentage :urbanity, :precision => 0
     length :weekly_distance_estimate
     price :weekly_cost_estimate
   end
@@ -36,20 +36,20 @@ describe ARMeasurementAttributes do
 
   it 'should save measurements in metric units' do
     automobile.urbanity.should be_close(0.74999999999999999999999, 0.001)
-    automobile.weekly_distance_estimate(:metric).should == 16.09344
+    automobile.weekly_distance_estimate.should == 16.09344
     automobile.weekly_cost_estimate.should == 45.0
 
-    boat.weekly_distance_estimate(:ugly).should == 20
+    boat.weekly_distance_estimate.should == 20
 
     candy.price.should == 12.0
   end
   it 'should display measurements in imperial units' do
-    automobile.urbanity(:pretty).should == "75%"
-    automobile.weekly_distance_estimate(:pretty => true).should == "10 miles"
-    automobile.weekly_cost_estimate(true).should == "$45.00"
+    automobile.pretty_urbanity(:precision => 0).should == "75%"
+    automobile.pretty_weekly_distance_estimate.should == "10mi"
+    automobile.pretty_weekly_cost_estimate.should == "$45.00"
 
-    boat.weekly_distance_estimate(:pretty).should == "10.8 nautical miles"
+    boat.pretty_weekly_distance_estimate.should == "10.8 nautical miles"
 
-    candy.price(:pretty).should == "$12.00"
+    candy.pretty_price.should == "$12.00"
   end
 end

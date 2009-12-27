3e63655b94b0923ec74f33570c1a4c9dbd384eb5
Derek Kastner <dkastner@gmail.com>
2009-12-27 14:58:46 -0500

    Finished Value class

--- lib/ar_measurement_attributes/value.rb
+++ lib/ar_measurement_attributes/value.rb
@@ -1,24 +1,44 @@
+require 'active_resource'
+require 'conversions'
+
 module ARMeasurementAttributes
   class Value
     attr_accessor :measurement, :internal_value, :options
 
     def initialize(measurement, internal_value, options)
       self.measurement = measurement
-      self.internal_value = internalvalue
-      self.options = options
+      self.internal_value = internal_value
+      self.options = (options || {})
+    end
+
+    def options=(val)
+      default_options = ARMeasurementAttributes.measurement_options_for(measurement)
+      if val === true || val == :pretty
+        @options = default_options.merge({ :pretty => true })
+      else
+        @options = default_options.merge(val)
+      end
     end
     
-    def convert_measurement(measurement, value, options)
+    def convert_measurement
+      return internal_value if options[:internal].nil? || options[:external].nil?
+
+      scale = options[:precision] || options[:scale]
+      args = [options[:internal], options[:external]]
+      args << { :scale => scale } if scale
+      internal_value.convert(*args)
     end
 
-    def format_measurement(value, options)
-    end
+    def label_measurement(value)
+      return value.to_s if options[:external].nil?
 
-    def label_measurement(measurement, value, options)
+      label = ARMeasurementAttributes.label_for(options[:external])
+
+      options[:prefix] ? "#{label}#{value}" : "#{value}#{label}"
     end
 
     def output
-      if runtime_options === true || runtime_options[:pretty]
+      if options[:pretty]
         to_s
       else
         internal_value
@@ -26,9 +46,8 @@ module ARMeasurementAttributes
     end
 
     def to_s
-      converted_value = convert_measurement(measurement, stored_value, options)
-      formatted_value = format_measurement(converted_value, options)
-      labeled_value = label_measurement(measurement, formatted_value, options)
+      converted_value = convert_measurement
+      label_measurement(converted_value)
     end
   end
 end
--- spec/ar_measurement_attributes/value_spec.rb
+++ spec/ar_measurement_attributes/value_spec.rb
@@ -5,25 +5,90 @@ describe ARMeasurementAttributes::Core do
     length_options = { :internal => :kilometres, :external => :miles }
     ARMeasurementAttributes::DEFAULT_MEASUREMENTS[:length] = length_options
   end
+  before(:each) do
+    @value = ARMeasurementAttributes::Value.new(:length, 15, {})
+  end
 
   describe :output do
+    it 'should return the raw data by default' do
+      @value.should_receive(:internal_value)
+      @value.output
+    end
+    it 'should return the string representation if the :pretty option is given' do
+      @value.options = :pretty
+      @value.should_receive(:to_s)
+      @value.output
+    end
+    it 'should return the string representation if the options param is true' do
+      @value.options = true
+      @value.should_receive(:to_s)
+      @value.output
+    end
+    it 'should return the string representation if the options param is a hash containing :pretty => true' do
+      @value.options = { :pretty => true }
+      @value.should_receive(:to_s)
+      @value.output
+    end
+  end
+
+  describe :convert_measurement do
+    it 'should convert the internal value from its internal representation into the external representation' do
+      @value.internal_value = 18.888883
+      @value.convert_measurement.should be_close(11.737, 0.001)
+    end
+    it 'should not do any conversion if the formats given are nil' do
+      @value.options = { :internal => nil, :external => nil }
+      @value.internal_value = 18.8
+      @value.convert_measurement.should == 18.8
+    end
+    it 'should include a scale argument to the call to conversion if precision is specified' do
+      @value.options[:precision] = 1
+      mock_fixnum = mock(Fixnum)
+      @value.stub!(:internal_value).and_return(mock_fixnum)
+      mock_fixnum.should_receive(:convert).
+        with(:kilometres, :miles, { :scale => 1 })
+
+      @value.convert_measurement
+    end
+    it 'should format the value with a given precision' do
+      @value.options = { :precision => 4 }
+      @value.internal_value = 19.23232323
+      @value.convert_measurement.should == 11.9504
+    end
+  end
+
+  describe :label_measurement do
+    it 'should add a label to the end of the measurement' do
+      @value.label_measurement(23).should == '23mi'
+    end
+    it 'should prefix a label if the :prefix option is true' do
+      @value.options = { :prefix => true }
+      @value.label_measurement(23).should == 'mi23'
+    end
+    it 'should not label the value if the external representation is nil' do
+      @value.options = { :external => nil }
+      @value.label_measurement(23).should == '23'
+    end
+  end
+
+  context 'integration' do
     before(:each) do
-      @value = ARMeasurementAttributes::Value.new(:length, 15, {})
+      @value.options[:precision] = 2
     end
     it 'should return the raw data by default' do
       @value.output.should == 15
     end
     it 'should return the pretty-formatted data if the :pretty option is given' do
       @value.options = :pretty
-      @value.output.should == '15mi'
+      @value.output.should == '9.32056788356001mi'
     end
     it 'should return the pretty-formatted data if the options param is true' do
       @value.options = true
-      @value.output.should == '15mi'
+      @value.output.should == '9.32056788356001mi'
     end
     it 'should return the pretty-formatted data if the options param is a hash containing :pretty => true' do
-      @value.options = { :params => true }
-      @value.output.should == '15mi'
+      @value.options[:pretty] = true
+      @value.output.should == '9.32mi'
     end
   end
 end

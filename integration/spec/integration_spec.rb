4c6f0ab026163d03990a4e944860c2ee45081ce8
Derek Kastner <dkastner@gmail.com>
2009-12-27 21:15:27 -0500

    Improved integration spec

--- integration/spec/integration_spec.rb
+++ integration/spec/integration_spec.rb
@@ -14,7 +14,8 @@ describe ARMeasurementAttributes do
   end
 
   class Boat < ActiveRecord::Base
-    length :weekly_distance_estimate, :external => :nautical_miles # override default external unit
+    length :weekly_distance_estimate, :precision => 2
+    mass :displacement, :external => :tons, :precision => 0
   end
 
   class Candy < ActiveRecord::Base
@@ -24,11 +25,11 @@ describe ARMeasurementAttributes do
   def automobile
     @automobile ||= Automobile.create(
       :urbanity => 0.74999999999999999999999,
-      :weekly_distance_estimate => 16.09344,
-      :weekly_cost_estimate => 45.0)
+      :weekly_distance_estimate => 16.0,
+      :weekly_cost_estimate => 45.00)
   end
   def boat
-    @boat ||= Boat.create(:weekly_distance_estimate => 20)
+    @boat ||= Boat.create(:weekly_distance_estimate => 20, :displacement => 8937)
   end
   def candy
     @candy ||= Candy.create(:price => 12)
@@ -36,20 +37,30 @@ describe ARMeasurementAttributes do
 
   it 'should save measurements in metric units' do
     automobile.urbanity.should be_close(0.74999999999999999999999, 0.001)
-    automobile.weekly_distance_estimate.should == 16.09344
+    automobile.weekly_distance_estimate.should == 16.0
     automobile.weekly_cost_estimate.should == 45.0
 
     boat.weekly_distance_estimate.should == 20
 
-    candy.price.should == 12.0
+    candy.price.should == 12.00
   end
   it 'should display measurements in imperial units' do
     automobile.pretty_urbanity(:precision => 0).should == "75%"
-    automobile.pretty_weekly_distance_estimate.should == "10mi"
-    automobile.pretty_weekly_cost_estimate.should == "$45.00"
+    automobile.urbanity = 0.75234
+    automobile.pretty_urbanity(:precision => 3).should == "75.234%"
+    automobile.pretty_weekly_distance_estimate.should == "9.94193907579734mi"
+    automobile.pretty_weekly_cost_estimate.should == "$45"
 
-    boat.pretty_weekly_distance_estimate.should == "10.8 nautical miles"
+    automobile.weekly_cost_estimate = 45.87
+    automobile.pretty_weekly_cost_estimate.should == "$45.87"
+
+    boat.pretty_weekly_distance_estimate.should == "12.43mi"
+    boat.pretty_displacement.should == "9 tons"
 
     candy.pretty_price.should == "$12.00"
+  end
+  it 'should correctly handle nil values' do
+    automobile.urbanity = nil
+    automobile.pretty_urbanity.should be_nil
   end
 end
